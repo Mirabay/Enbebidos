4fed91ee66f73bf492f1ffab5ae29ef288d43d03
Toño <tonyshauder@gmail.com>
2023-09-19 22:24:01 -0600

    Ya termine

--- exercise03.cpp
+++ exercise03.cpp
@@ -5,4 +5,40 @@
     manera ascendente (de menor a mayor) usando el algoritmo 
     de ordenamiento conocido como Enumeration Sort.
 */
+#include <iostream>
 
+
+int* enumerationSort(int a[],int b){
+    
+    int *arrAux = new int [b];  // Creamos un arreglo auxiliar 
+    // Por cada posición i en el arreglo
+    for (size_t i = 0; i < b; i++)
+    {
+        // Inicializamos un contador en cero
+        int count= 0;
+        // Por cada posición j en el arreglo
+        for (size_t j = 0; j < b; j++)
+        {
+            // Si a[i] > a[j], incrementa el contador
+            if (a[i]>a[j])
+            {
+                count++;
+            }
+            // Sino
+            else if (a[i]==a[j] && j<i) // Si a[i] == a[j] y j < i, incrementa el contador
+            {
+                count++;
+            } 
+        }
+        // Copia a[i] a b[contador]
+        arrAux[count]=a[i];
+    }
+    // Copia el arreglo b al arreglo a
+    a = arrAux;
+    return a;
+}
+int main(int argc, char const *argv[])
+{
+    
+    return 0;
+}
