dff09d58cd20d5ba45449068208a7193e482353f
Toño <tonyshauder@gmail.com>
2023-09-19 21:36:21 -0600

    Ya solo falta el 3

--- exercise02.cpp
+++ exercise02.cpp
@@ -9,19 +9,44 @@ El resultado esperado para el límite de
 el tipo de datos).
 */
 
-int exercise02(int a,int b)
-{
-    int sum=0;
-    if (a== 0 or a== 1 or a== 4) sum=sum;
-    for (int x = 2; x < a / 2; x++) {
-    if (a % x == 0) return false;
+#include <iostream>
+#include <vector>
+
+bool esPrimo(int numero) {
+
+    /*
+        No entendi el algoritmo de peter
+        Pero GeeksForGeeks nunca me abandona
+        https://www.geeksforgeeks.org/check-number-full-prime/
+    */
+    
+    if (numero == 1)
+        return false;
+ 
+    // check for all factors
+    for (int i = 2; i * i <= numero; i++) {
+        if (numero % i == 0)
+            return false;
     }
-    
-    
+ 
+    return true;
 }
 
-int main(int argc, char const *argv[])
-{
-    /* code */
+long long sumaPrimos( int b) {
+    long long suma = 0;
+
+    for (int i = 2; i <= b; ++i) {
+        if (esPrimo(i)) {
+            suma += i;
+        }
+    }
+
+    return suma;
+}
+
+int main() {
+    int b= 5000000;
+    long long resultado = sumaPrimos(b);
+    std::cout << "La suma de los números primos hasta "<< b << " es: " << resultado << std::endl;
     return 0;
 }
---
+++ exercise03.cpp
@@ -0,0 +1,8 @@
+/*
+    Desarrolla un procedimiento que recibe como parámetros
+    un arreglo de enteros y el tamaño del mismo. 
+    El procedimiento ordena los elementos del arreglo de 
+    manera ascendente (de menor a mayor) usando el algoritmo 
+    de ordenamiento conocido como Enumeration Sort.
+*/
+
